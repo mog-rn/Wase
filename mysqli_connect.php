5eb2d13e9114f361cef92f7eff5e25bcdcbfd8da
Proffesional Center <gituser@localhost.localdomain>
2020-05-05 21:57:34 +0300

    Updated the database connection

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,1 +1,27 @@
-$sql = "CREATE USER \'mogaka\'@\'localhost\' IDENTIFIED VIA mysql_native_password USING \'***\'GRANT USAGE ON *.* TO \'mogaka\'@\'localhost\' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0";
+/*$sql = "CREATE USER \'mogaka\'@\'localhost\' IDENTIFIED VIA mysql_native_password USING \'***\'GRANT USAGE ON *.* TO \'mogaka\'@\'localhost\' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0";*/
+
+<?php
+//Conections to the database and MySQL.
+//Constants:
+define('DB_USER', 'Mogaka');
+define('DB_PASSWORD', 'mogakafor hacking');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'wasifyads');
+
+//Database connection
+try {
+    $dbcon = new mysqli(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD);
+    mysqli_set_charset($dbcon, 'utf8');
+    //More code goes here...
+} catch (Exception $e) {
+    //print "An Exception occurred. Message:" . $e->getMessage();
+    print "The System is busy please try again later.";
+}
+catch (Error $e){
+    //print "An Error occurred. Message: " . $e->getMessage();
+    print "The System is busy please try again later";
+}
+?>
+
+
+
